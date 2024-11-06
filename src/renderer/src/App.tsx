9e60f9fb54c40097316836b34f2be9ea7e4ffa74
terrence-ou <outerrencedl@gmail.com>
2024-11-06 11:17:36 -0500

    project config updated

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,29 +1,14 @@
 import Versions from "./components/Versions";
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send("ping");
+  //const ipcHandle = (): void => window.electron.ipcRenderer.send("ping");
 
   return (
     <>
-      <div className="text-lg">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
+      <div>
+        <h1 className="text-xl font-bold text-zinc-700">
+          Electron + React + Shadcn
+        </h1>
       </div>
       <Versions></Versions>
     </>
