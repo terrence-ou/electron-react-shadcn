cafad07ab2a079b6e06c774fe464b27dc3364983
terrence-ou <outerrencedl@gmail.com>
2024-11-11 14:54:34 -0500

    coverage exclusion updated

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { JestConfigWithTsJest } from "ts-jest";
 
 const config: JestConfigWithTsJest = {
   testEnvironment: "jsdom",
-  roots: ["src/renderer/src/__test__"],
+  roots: ["src/renderer/src/__test__"], // your unit test folders
   preset: "ts-jest",
   transform: {
     "^.+\\.tsx?$": [
@@ -17,9 +17,11 @@ const config: JestConfigWithTsJest = {
     "^@shared/(.*)$": "<rootDir>/src/shared/$1",
     "\\.svg$": "<rootDir>/src/renderer/src/__test__/utils/svg.mock.ts", // mocks SVG
   },
+  // adding ignore paths to exlude them from coverages
   coveragePathIgnorePatterns: [
     "<rootDir>/node_modules/",
     "<rootDir>/src/renderer/src/__test__/utils/",
+    "<rootDir>/src/renderer/src/components/ui/", // the Shadcn UI folder
   ],
 };
 
